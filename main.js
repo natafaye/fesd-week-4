a5bcb326bb74fb76456643f6ef60afa14bf24d46
Natalie Childs <nata.faye@gmail.com>
2024-05-22 20:06:14 -0700

    learned about variables

---
+++ main.js
@@ -0,0 +1,38 @@
+
+
+// we will set variables to user input
+var order = "tacos"
+var tacoAmount = 5
+var toGo = false
+
+var total = tacoAmount * 3
+
+console.log(order)
+
+var order2 = "burritos"
+var burritoAmount = 2
+var toGo2 = true
+
+total = total + burritoAmount * 4
+
+console.log(order)
+
+// friend = "hippocuppup"
+
+var friend = "fish"
+friend = "hippo"
+
+friend += "pup cup tuna"
+// friend = friend + "cup"
+// friend = "hippopup cup tuna"
+
+
+var animal = "hippo"
+
+console.log(friend + animal)
+
+
+
+
+//don't
+//do not
